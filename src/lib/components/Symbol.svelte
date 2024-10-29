022382b8ae6c6a6f538b1b152d01f39236bf7f42
zorvi <aervxa@gmail.com>
2024-10-29 08:17:04 +0300

    Updated symbols component

--- src/lib/components/Symbol.svelte
+++ src/lib/components/Symbol.svelte
@@ -1,6 +1,11 @@
+<!--
+@component
+name, size, fill, wght, grad
+-->
+
 <script>
 	/**
-	 * @typedef {Object} Props
+	 * @typedef Props
 	 * @property {string} name The symbol's name. (https://fonts.google.com/icons?icon.style=Rounded)
 	 * @property {`${20 | 24 | 40 | 48}`} [size] The symbol's size in px. default: `24`
 	 * @property {`${boolean}`} [fill] Use `false` for **outlined** symbols (default) or `true` for *completely* **filled** symbols.
@@ -8,15 +13,24 @@
 	 * @property {`${-25 | 0 | 200}`} [grad] The symbol stroke's emphasis. **Low** emphasis (-25), default (0), and  **high** emphasis (200)
 	 */
 
-	/** @type {Props} */
-	const { name, size = '24', fill = 'false', wght = '400', grad = '0' } = $props();
+	/** @type {Props & import('svelte/elements').HTMLAttributes<HTMLSpanElement>} */
+	const {
+		name,
+		size = '24',
+		fill = 'false',
+		wght = '400',
+		grad = '0',
+		class: className,
+		...props
+	} = $props();
 </script>
 
 <span
 	style="width: {size}px; font-size: {size}px; font-variation-settings: 'FILL' {JSON.parse(fill)
 		? 1
 		: 0}, 'wght' {wght}, 'GRAD' {grad}, 'opsz' {size}"
-	class="font-material-symbols-rounded block aspect-square select-none overflow-clip leading-none"
+	class="block aspect-square select-none overflow-clip font-material-symbols-rounded leading-none {className}"
+	{...props}
 >
 	{name}
 </span>
